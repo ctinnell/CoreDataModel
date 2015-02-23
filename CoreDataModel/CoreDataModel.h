924318d3ef2e57c93217969477690a6b828c441b
Clay Tinnell <ctinnell@gaig.com>
2015-02-23 12:26:48 -0500

    Initial Commit

---
+++ CoreDataModel/CoreDataModel.h
@@ -0,0 +1,19 @@
+//
+//  CoreDataModel.h
+//  CoreDataModel
+//
+//  Created by Clay Tinnell on 2/23/15.
+//  Copyright (c) 2015 Clay Tinnell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CoreDataModel.
+FOUNDATION_EXPORT double CoreDataModelVersionNumber;
+
+//! Project version string for CoreDataModel.
+FOUNDATION_EXPORT const unsigned char CoreDataModelVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CoreDataModel/PublicHeader.h>
+
+
