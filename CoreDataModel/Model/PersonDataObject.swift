250b33e113b9a06f634f9ed3a9317f4e06e52cd9
Clay Tinnell <ctinnell@gaig.com>
2015-02-24 18:41:37 -0500

    load person test data

--- CoreDataModel/Model/PersonDataObject.swift
+++ CoreDataModel/Model/PersonDataObject.swift
@@ -11,4 +11,28 @@ import CoreData
 
 extension Person {
     
+    class func entityName() -> String {
+        return "Person"
+    }
+    
+    class func loadTestData(moc: NSManagedObjectContext) {
+        let fetchRequest = NSFetchRequest(entityName: entityName())
+        let entityDescription = NSEntityDescription.entityForName(entityName(), inManagedObjectContext: moc)
+        fetchRequest.entity = entityDescription
+        var error: NSError?
+        let results = moc.executeFetchRequest(fetchRequest, error: &error) as [Person]
+        if countElements(results) == 0 {
+            createPerson("Bill Gates", email: "billgates@microsoft.com", moc: moc)
+            createPerson("Tim Cook", email: "timcook@apple.com", moc: moc)
+        }
+    }
+    
+    class func createPerson(name: String, email: String, moc: NSManagedObjectContext) {
+        let person = NSEntityDescription.insertNewObjectForEntityForName(entityName(), inManagedObjectContext: moc) as Person
+        
+        var error: NSError?
+        person.name = name
+        person.email = email
+        moc.save(&error)
+    }
 }
